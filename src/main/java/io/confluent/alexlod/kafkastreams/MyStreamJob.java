e359a9e3948ef09bbef720f3cdad6bb019aaaf64
Alex Loddengaard <alexloddengaard@gmail.com>
2016-11-15 16:45:02 -0800

    Querying the state store from within the stream application. Still need to query from outside the application.

--- src/main/java/io/confluent/alexlod/kafkastreams/MyStreamJob.java
+++ src/main/java/io/confluent/alexlod/kafkastreams/MyStreamJob.java
@@ -11,11 +11,14 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -30,6 +33,10 @@ public class MyStreamJob {
   public static final String STREAMS_LOCAL_DIR = "/tmp/kafka-streams";
 
   public static final String OUTPUT_TOPIC = "url-region-clicks";
+
+  public static final String OUTPUT_STATE_STORE_NAME = "ClicksPerRegionUnwindowed";
+
+  public static final long SLEEP_BETWEEN_STATE_STORE_QUERIES_MS = 10000;
 
   private static final Logger log = LoggerFactory.getLogger(MyStreamJob.class);
 
@@ -63,7 +70,7 @@ public class MyStreamJob {
             .leftJoin(userLocationsTable, (url, region) -> new UrlRegionClicks(url, region))
             .map((userId, regionUrlClicks) -> new KeyValue<String, UrlRegionClicks>(regionUrlClicks.getUrl(), regionUrlClicks))
             .groupByKey(stringSerde, regionUrlClicksSerde)
-            .reduce((firstClicks, secondClicks) -> firstClicks.combine(secondClicks), "ClicksPerRegionUnwindowed") // the lambda could be replaced with `RegionUrlClicks::combine`.
+            .reduce((firstClicks, secondClicks) -> firstClicks.combine(secondClicks), OUTPUT_STATE_STORE_NAME) // the lambda could be replaced with `RegionUrlClicks::combine`.
             .to(stringSerde, regionUrlClicksSerde, OUTPUT_TOPIC);
 
     KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
@@ -75,6 +82,21 @@ public class MyStreamJob {
         deleteState();
       }
     });
+
+    while (true) {
+      try {
+        Thread.sleep(SLEEP_BETWEEN_STATE_STORE_QUERIES_MS);
+      } catch (InterruptedException ie) {
+        // do nothing.
+      }
+      ReadOnlyKeyValueStore<String, UrlRegionClicks> keyValueStore = streams.store(OUTPUT_STATE_STORE_NAME, QueryableStoreTypes.keyValueStore());
+      UrlRegionClicks clicks = keyValueStore.get("http://foo.bar/3");
+      Map<String, Long> regionClicks = clicks.getRegionClicks();
+      for (String key : regionClicks.keySet()) {
+        log.info(key + ": " + regionClicks.get(key));
+      }
+      log.info("");
+    }
   }
 
   private static void deleteState() {
